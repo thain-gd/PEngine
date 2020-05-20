c7b438f93d272b82a8a0e6cb887dc1327fa6d6d3
TNGDev <nqcuong2@gmail.com>
2020-05-20 11:50:25 -0500

    Updated premake file to include PEngine src in include dir also

--- PEngine/src/PEngine/Events/Event.h
+++ PEngine/src/PEngine/Events/Event.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "Core.h"
+#include "PEngine/Core.h"
 
 #include <string>
 #include <functional>
--- premake5.lua
+++ premake5.lua
@@ -26,13 +26,14 @@ project "PEngine"
 
     includedirs
     {
+        "%{prj.name}/src",
         "%{prj.name}/vendor/spdlog/include"
     }
 
     filter "system:windows"
         cppdialect "C++17"
         staticruntime "On"
-        systemversion "10.0.17763.0"
+        systemversion "latest"
 
         defines
         {
@@ -85,7 +86,7 @@ project "SandBox"
     filter "system:windows"
         cppdialect "C++17"
         staticruntime "On"
-        systemversion "10.0.17763.0"
+        systemversion "latest"
 
         defines
         {
