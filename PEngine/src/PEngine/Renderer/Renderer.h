8d8d672663953437df8400012c62beaa091feb22
TNGDev <nqcuong2@gmail.com>
2020-06-14 16:55:30 -0500

    Added transform to render objects with specific locations/scales

--- PEngine/src/PEngine/Renderer/Renderer.h
+++ PEngine/src/PEngine/Renderer/Renderer.h
@@ -20,7 +20,7 @@ namespace PEngine
 		static void BeginScene(OrthographicCamera& camera);
 		static void EndScene();
 
-		static void Submit(const std::shared_ptr<Shader>& shader, const std::shared_ptr<VertexArray>& vertexArray);
+		static void Submit(const std::shared_ptr<Shader>& shader, const std::shared_ptr<VertexArray>& vertexArray, const glm::mat4& transform = glm::mat4(1));
 
 		static RendererAPI::API GetAPI() { return RendererAPI::GetAPI(); }
 	};
