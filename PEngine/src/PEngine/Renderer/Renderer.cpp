9c3126d1059a5c3a41d3c85581da8940bb1f42a8
TNGDev <nqcuong2@gmail.com>
2020-06-16 15:21:36 -0500

    Abstracted Shader with OpenGLShader implemented

--- PEngine/src/PEngine/Renderer/Renderer.cpp
+++ PEngine/src/PEngine/Renderer/Renderer.cpp
@@ -1,6 +1,8 @@
 #include "pepch.h"
 #include "Renderer.h"
 
+#include "Platforms/OpenGL/OpenGLShader.h"
+
 namespace PEngine
 {
 	Renderer::SceneData* Renderer::m_SceneData = new Renderer::SceneData;
@@ -17,8 +19,9 @@ namespace PEngine
 	void Renderer::Submit(const std::shared_ptr<Shader>& shader, const std::shared_ptr<VertexArray>& vertexArray, const glm::mat4& transform)
 	{
 		shader->Bind();
-		shader->UploadUniformMat4("u_ViewProjection", m_SceneData->ViewProjectionMatrix);
-		shader->UploadUniformMat4("u_Transform", transform);
+		// Will change to be fully abstract later when adding more Shader types
+		std::dynamic_pointer_cast<OpenGLShader>(shader)->UploadUniformMat4("u_ViewProjection", m_SceneData->ViewProjectionMatrix);
+		std::dynamic_pointer_cast<OpenGLShader>(shader)->UploadUniformMat4("u_Transform", transform);
 
 		vertexArray->Bind();
 		RenderCommand::DrawIndexed(vertexArray);
