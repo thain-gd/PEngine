212ab6bf7e0162c244b442e4f01e5fd056f71f58
TNGDev <nqcuong2@gmail.com>
2020-07-01 00:44:10 -0500

    Updated basic sandbox to interact with the camera controller

--- SandBox/src/SanboxApp.cpp
+++ SandBox/src/SanboxApp.cpp
@@ -1,20 +1,213 @@
 #include <PEngine.h>
 
+#include "Platforms/OpenGL/OpenGLShader.h"
+
+#include "imgui/imgui.h"
+
+#include "glm/gtc/matrix_transform.hpp"
+#include <glm/gtc/type_ptr.hpp>
+
 class ExampleLayer : public PEngine::Layer
 {
+private:
+	PEngine::ShaderLibrary m_ShaderLibrary;
+	PEngine::Ref<PEngine::Shader> m_Shader;
+	PEngine::Ref<PEngine::VertexArray> m_VertexArray;
+
+	PEngine::Ref<PEngine::Shader> m_FlatColorShader;
+	PEngine::Ref<PEngine::VertexArray> m_SquareVA;
+
+	PEngine::Ref<PEngine::Texture2D> m_Texture, m_LogoTexture;
+
+	PEngine::OrthographicCameraController m_CameraController;
+
+	glm::vec3 m_SquareColor = { 0.2f, 0.3f, 0.8f };
+
 public:
 	ExampleLayer()
-		: Layer("Example") {}
-
-	void OnUpdate() override
+		: Layer("Example"), m_CameraController(1280.0f / 720.0f)
 	{
-		
+		m_VertexArray = PEngine::VertexArray::Create();
+
+		float vertices[3 * 7] = {
+			-0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f,
+			 0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f,
+			 0.0f,  0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f
+		};
+
+		PEngine::Ref<PEngine::VertexBuffer> vertexBuffer = PEngine::VertexBuffer::Create(vertices, sizeof(vertices));
+		PEngine::BufferLayout layout = {
+			{ PEngine::ShaderDataType::Float3, "a_Position" },
+			{ PEngine::ShaderDataType::Float4, "a_Color" }
+		};
+		vertexBuffer->SetLayout(layout);
+		m_VertexArray->AddVertexBuffer(vertexBuffer);
+
+		uint32_t indices[3] = { 0, 1, 2 };
+		PEngine::Ref<PEngine::IndexBuffer> indexBuffer = PEngine::IndexBuffer::Create(indices, sizeof(indices) / sizeof(uint32_t));
+		m_VertexArray->SetIndexBuffer(indexBuffer);
+
+		m_SquareVA = PEngine::VertexArray::Create();
+
+		float squareVertices[5 * 4] = {
+			-0.5f, -0.5f, 0.0f, 0.0f, 0.0f,
+			 0.5f, -0.5f, 0.0f, 1.0f, 0.0f,
+			 0.5f,  0.5f, 0.0f, 1.0f, 1.0f,
+			-0.5f,  0.5f, 0.0f, 0.0f, 1.0f
+		};
+
+		PEngine::Ref<PEngine::VertexBuffer> squareVB = PEngine::VertexBuffer::Create(squareVertices, sizeof(squareVertices));
+		squareVB->SetLayout({
+			{ PEngine::ShaderDataType::Float3, "a_Position" },
+			{ PEngine::ShaderDataType::Float2, "a_TexCoord" }
+		});
+		m_SquareVA->AddVertexBuffer(squareVB);
+
+		uint32_t squareIndices[6] = { 0, 1, 2, 2, 3, 0 };
+		PEngine::Ref<PEngine::IndexBuffer> squareIB = PEngine::IndexBuffer::Create(squareIndices, sizeof(squareIndices) / sizeof(uint32_t));
+		m_SquareVA->SetIndexBuffer(squareIB);
+
+		std::string vertexSrc = R"(
+			#version 330 core
+
+			layout(location = 0) in vec3 a_Position;
+			layout(location = 1) in vec4 a_Color;
+
+			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
+
+			out vec3 v_Position;
+			out vec4 v_Color;
+
+			void main()
+			{
+				v_Position = a_Position;
+				v_Color = a_Color;
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
+			}
+		)";
+
+		std::string fragmentSrc = R"(
+			#version 330 core
+
+			layout(location = 0) out vec4 color;
+
+			in vec3 v_Position;
+			in vec4 v_Color;
+
+			void main()
+			{
+				color = vec4(v_Position * 0.5 + 0.5, 1.0);
+				color = v_Color;
+			}
+		)";
+
+		m_Shader = PEngine::Shader::Create("VertexPosColor", vertexSrc, fragmentSrc);
+
+		std::string flatColorShaderVertexSrc = R"(
+			#version 330 core
+
+			layout(location = 0) in vec3 a_Position;
+
+			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
+
+			out vec3 v_Position;
+			out vec4 v_Color;
+
+			void main()
+			{
+				v_Position = a_Position;
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
+			}
+		)";
+
+		std::string flatColorShaderFragmentSrc = R"(
+			#version 330 core
+
+			layout(location = 0) out vec4 color;
+
+			in vec3 v_Position;
+			
+			uniform vec3 u_Color;
+
+			void main()
+			{
+				color = vec4(u_Color, 1.0);
+			}
+		)";
+
+		m_FlatColorShader = PEngine::Shader::Create("FlatColor", flatColorShaderVertexSrc, flatColorShaderFragmentSrc);
+
+		auto textureShader = m_ShaderLibrary.Load("assets/shaders/Texture.glsl");
+
+		m_Texture = PEngine::Texture2D::Create("assets/textures/Checkerboard.png");
+		m_LogoTexture = PEngine::Texture2D::Create("assets/textures/ChernoLogo.png");
+		std::dynamic_pointer_cast<PEngine::OpenGLShader>(textureShader)->Bind();
+		std::dynamic_pointer_cast<PEngine::OpenGLShader>(textureShader)->UploadUniformInt("u_Texture", 0);
 	}
+
+	void OnUpdate(PEngine::Timestep ts) override
+	{
+		// Update
+		m_CameraController.OnUpdate(ts);
+
+		// Render
+		PEngine::RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1 });
+		PEngine::RenderCommand::Clear();
+
+		PEngine::Renderer::BeginScene(m_CameraController.GetCamera());
+
+		glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
+
+		glm::vec4 redColor(0.8f, 0.2f, 0.3f, 1.0f);
+		glm::vec4 blueColor(0.2f, 0.3f, 0.8f, 1.0f);
+
+		std::dynamic_pointer_cast<PEngine::OpenGLShader>(m_FlatColorShader)->Bind();
+		std::dynamic_pointer_cast<PEngine::OpenGLShader>(m_FlatColorShader)->UploadUniformFloat3("u_Color", m_SquareColor);
+
+		for (int y = 0; y < 20; y++)
+		{
+			for (int x = 0; x < 20; x++)
+			{
+				glm::vec3 pos(y * 0.11f, x * 0.11f, 0.0f);
+				glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
+				PEngine::Renderer::Submit(m_FlatColorShader, m_SquareVA, transform);
+			}
+		}
+
+		auto textureShader = m_ShaderLibrary.Get("Texture");
+		m_Texture->Bind();
+		PEngine::Renderer::Submit(textureShader, m_SquareVA, glm::scale(glm::mat4(1.0f), glm::vec3(1.5f)));
+		m_LogoTexture->Bind();
+		PEngine::Renderer::Submit(textureShader, m_SquareVA, glm::scale(glm::mat4(1.0f), glm::vec3(1.5f)));
+
+		//Triangle
+		//PEngine::Renderer::Submit(m_Shader, m_VertexArray);
+
+		PEngine::Renderer::EndScene();
+	}
+
+	void OnImGuiRender() override
+	{
+		ImGui::Begin("Settings");
+		ImGui::ColorEdit3("Square Color", glm::value_ptr(m_SquareColor));
+		ImGui::End();
 	}
 
 	void OnEvent(PEngine::Event& e) override
 	{
+		m_CameraController.OnEvent(e);
+	}
 
+	bool OnKeyPressedEvent(PEngine::KeyPressedEvent& event)
+	{
+		if (event.GetKeyCode() == PE_KEY_LEFT)
+		{
+
+		}
+
+		return false;
 	}
 };
 
