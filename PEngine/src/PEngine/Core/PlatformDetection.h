250076d0babd8edbd39e5baae85f22ae4c663f6b
Thai Nguyen <nqcuong2@gmail.com>
2020-12-05 21:17:28 -0600

    Defined NOMINMAX to prevent bugs with imgui compiling

--- PEngine/src/PEngine/Core/PlatformDetection.h
+++ PEngine/src/PEngine/Core/PlatformDetection.h
@@ -5,7 +5,7 @@
 	/* Windows x64/x86 */
 	#ifdef _WIN64
 		/* Windows x64  */
-		#define HZ_PLATFORM_WINDOWS
+		#define PENGINE_PLATFORM_WINDOWS
 	#else
 		/* Windows x86 */
 		#error "x86 Builds are not supported!"
